259528c02c2195c5f35ca52484456072fddb9321
Edwin3002 <vargas11742@gmail.com>
2022-10-07 17:12:16 -0500

    types of data

---
+++ dart/tiposDatos.dart
@@ -0,0 +1,30 @@
+void main(){
+var name = 'paco';
+var age = 19;
+var dev = true;
+print(name);
+print(name.runtimeType);
+print(age.runtimeType);
+print(dev.runtimeType);
+ rice();
+}
+
+void rice(){
+  String name = 'pepe';
+  int age = 32;
+  double money = 57.3;
+  bool dev = false;
+  double peso;
+  peso = 90.2;
+
+  final lastName = 'ayala';
+  const month = 12;
+
+  print(name);
+  print(age);
+  print(money);
+  print(dev);
+  print(peso);
+  print(lastName);
+  print(month);
+}
