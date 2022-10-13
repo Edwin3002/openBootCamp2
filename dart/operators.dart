4d85aa201d4ca30bd40ad5d53ae645d49322ff97
Edwin3002 <vargas11742@gmail.com>
2022-10-12 23:29:24 -0500

    arimeticos

---
+++ dart/operators.dart
@@ -0,0 +1,15 @@
+void main(){
+  int a = 10;
+  int b = 25;
+
+  print(a + b);
+  print(a - b);
+  print(a * b);
+  print(a / b);
+  print(a ~/ b);
+  print(a % b);
+  a--;
+  print(a);
+  b++;
+  print(b);
+}
