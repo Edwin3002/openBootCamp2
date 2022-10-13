0297999f4a0a04265e59eff4c7d33d5c67f23451
Edwin3002 <vargas11742@gmail.com>
2022-10-13 00:24:48 -0500

    if, else

---
+++ dart/condicionales.dart
@@ -0,0 +1,13 @@
+void main(){
+
+  var sky = 'blue';
+
+  if(sky == 'blue'){
+    print('cielo si es azul');    
+  }else if(sky == 'blanco'){
+    print('cielo no es azul, es blanco');
+  }else{
+    print('ni azul, ni blanco');
+
+  }
+}
