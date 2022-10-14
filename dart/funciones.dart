1849ab0032b2f7b5fa698cacad4be8e18795cd40
Edwin3002 <vargas11742@gmail.com>
2022-10-14 18:45:56 -0500

    funciones

---
+++ dart/funciones.dart
@@ -0,0 +1,19 @@
+void main(){
+    void mundo(){
+      print('hello world dart');
+    }
+    mundo();
+
+    int ageFunc(){
+      int age = 19;
+      return age;
+    }
+    print(ageFunc());
+    
+
+    double area(double h, double w){
+      return h * w;
+
+    }
+    print(area(4, 8));
+}
