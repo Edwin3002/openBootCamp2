64fee95ea69901c440e1c74d06478b6e6e3c3338
Edwin3002 <vargas11742@gmail.com>
2022-10-15 23:23:56 -0500

    poo2

--- dart/poo/class.dart
+++ dart/poo/class.dart
@@ -2,13 +2,15 @@ import 'cookies.dart';
 
 void main() {
   print('hola');
-  Cookie tom = Cookie('tomi', 40);
+  Cookie tom = Cookie('tomi', 40, 5);
   Cookie paco = Cookie.gluten('paco', 33, false);
   tom.nacer();
   tom.dormir();
   var name = tom.name;
   var age = tom.age;
-  print('tu galleta se llama $name y tiene $age años');
+  tom.setTime = 4;
+  var time = tom.getTime();
+  print('tu galleta se llama $name y tiene $age años y tiene $time min en el horno');
   print(paco.gluten);
 
   bool? glu = paco.gluten;
--- dart/poo/cookies.dart
+++ dart/poo/cookies.dart
@@ -3,13 +3,20 @@ class Cookie{
   String name ='Tom';
   int? age;
   bool? gluten;
+  int? _time;
 
-  Cookie(this.name, this.age){
+  Cookie(this.name, this.age, this._time){
     // this.name = name;
     // this.age = age;
   }
   Cookie.gluten(this.name, this.age, this.gluten){
   }
+
+  set setTime(int time){
+    _time = time;
+  }
+
+  int getTime() => _time!;
 
     void nacer(){
       print('estoy vivo?');
